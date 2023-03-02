1b9daedba901c9a155a217925117390380b5f967
André Schweizer <andre-tobias-schweizer@live.de>
2023-03-02 20:00:26 +0100

    Added basic rent table

--- src/components/ResidentInformation/RentInformation.tsx
+++ src/components/ResidentInformation/RentInformation.tsx
@@ -1,5 +1,9 @@
+import { Icon } from '@mui/material';
+import { MDBTable, MDBTableBody, MDBTableHead } from 'mdb-react-ui-kit';
 import React from 'react';
+import { v4 as uuid } from 'uuid';
 import { Resident } from '_/types/resident';
+import { convertCurrencyCentsToString } from '_/utils/currency';
 
 interface RentInformationProps {
   /**
@@ -13,23 +17,40 @@ interface RentInformationProps {
  */
 function RentInformation(props: RentInformationProps): JSX.Element {
   return (
-    <>
-      {props.resident.rent.map((rent) => (
-        <p>
-          {rent.dueDate.month}
-          {' '}
-          {rent.dueDate.year}
-          {' '}
-          -
-          {' '}
-          {rent.rent}
-          {' '}
-          -
-          {' '}
-          {String(rent.isPaid)}
-        </p>
-      ))}
-    </>
+    <MDBTable hover>
+      <MDBTableHead>
+        <tr>
+          <th scope="col">Monat</th>
+          <th scope="col">Miete</th>
+          <th scope="col">Bezahlt</th>
+          <th scope="col">Aktionen</th>
+        </tr>
+      </MDBTableHead>
+      <MDBTableBody>
+        {[...props.resident.rent].reverse().map((rent) => (
+          <tr key={uuid()}>
+            <td>{`${rent.dueDate.month} ${rent.dueDate.year}`}</td>
+            <td>{convertCurrencyCentsToString(rent.rent)}</td>
+            <td>
+              {rent.isPaid ? (
+                <Icon
+                  baseClassName="fa-regular"
+                  className="fa-circle-check"
+                  color="success"
+                />
+              ) : (
+                <Icon
+                  baseClassName="fa-regular"
+                  className="fa-circle-xmark"
+                  color="error"
+                />
+              )}
+            </td>
+            <td />
+          </tr>
+        ))}
+      </MDBTableBody>
+    </MDBTable>
   );
 }
 
