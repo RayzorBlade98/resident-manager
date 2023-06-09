6d8a3a065eb43d73792067ea1b44aa7401ed0256
André Schweizer <andre-tobias-schweizer@live.de>
2023-06-09 14:58:26 +0200

    Added AppWrapper component

---
+++ src/components/functional/AppWrapper.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+import RecoilNexus from 'recoil-nexus';
+import SaveStateManager from './SaveStateHandler/SaveStateHandler';
+import StandardLocalizationProvider from './StandardLocalizationProvider/StandardLocalizationProvider';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface AppWrapperProps {}
+
+/**
+ * Component that includes all functional components in which the app needs to be wrapped
+ */
+function AppWrapper(
+  props: React.PropsWithChildren<AppWrapperProps>,
+): JSX.Element {
+  return (
+    <RecoilRoot>
+      <RecoilNexus />
+      <SaveStateManager />
+      <StandardLocalizationProvider>
+        {props.children}
+      </StandardLocalizationProvider>
+    </RecoilRoot>
+  );
+}
+
+export default AppWrapper;
--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -6,21 +6,14 @@ import '_public/style.css';
 
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { RecoilRoot } from 'recoil';
-import RecoilNexus from 'recoil-nexus';
-import SaveStateManager from '_/components/functional/SaveStateHandler/SaveStateHandler';
-import StandardLocalizationProvider from '_/components/functional/StandardLocalizationProvider/StandardLocalizationProvider';
+import AppWrapper from '_/components/functional/AppWrapper';
 import App from '_renderer/App';
 
 const container = document.getElementById('app');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
 root.render(
-  <RecoilRoot>
-    <RecoilNexus />
-    <SaveStateManager />
-    <StandardLocalizationProvider>
-      <App />
-    </StandardLocalizationProvider>
-  </RecoilRoot>,
+  <AppWrapper>
+    <App />
+  </AppWrapper>,
 );
